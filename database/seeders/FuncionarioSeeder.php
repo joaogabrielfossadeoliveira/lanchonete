71841094f9da6b0a682efa4b0a0c6402179de37f
joaogabrielfossa <joaogabrielfo24@gmail.com>
2025-03-25 10:44:15 -0300

    funcionario

---
+++ database/seeders/FuncionarioSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Funcionario;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+class FuncionarioSeeder extends Seeder
+{
+    public function run()
+    {
+        Funcionario::create([
+            'nome' => 'Carlos Oliveira',
+            'cpf' => '12345678901',
+            'email' => 'carlos@email.com',
+            'password' => bcrypt('senha123'),
+            'role' => 'funcionario'
+        ]);
+
+        Funcionario::create([
+            'nome' => 'Ana marques',
+            'cpf' => '98765432100',
+            'email' => 'ana@email.com',
+            'password' => bcrypt('senha123'),
+            'role' => 'admin'
+        ]);
+    }
+}
